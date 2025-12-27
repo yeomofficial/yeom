24edf76142cf47a12dfab2cf1fda9bc29e93b0a6
yeomofficial <yeomapp@gmail.com>
2025-12-27 11:55:45 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+alert("LOGIN JS LOADED");
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
