e44d981d170b645e5000b55fc7f05123ab1b482b
yeomofficial <yeomapp@gmail.com>
2025-12-27 15:09:27 +0530

    Create fbase.js

---
+++ fbase.js
@@ -0,0 +1,18 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1O-WVb95Z77o2JelptaZ8ljRPdNVDIeY",
+  authDomain: "yeom-official.firebaseapp.com",
+  projectId: "yeom-official",
+  storageBucket: "yeom-official.firebasestorage.app",
+  messagingSenderId: "285438640273",
+  appId: "1:285438640273:web:7d91f4ddc24536a3c5ff30",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
