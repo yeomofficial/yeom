ab6ceb5bd264b8b8aef4f9e271665bf6019d03bc
yeomofficial <yeomapp@gmail.com>
2026-01-04 12:11:35 +0530

    Refactor post creation and loading logic

--- home.js
+++ home.js
@@ -36,7 +36,7 @@ function createPost({ username, imageUrl }) {
       <span class="username">${username}</span>
 
       <button class="post-menu" aria-label="Post options">
-        <img src="dots.png" alt="More options" />
+        <img src="three-dots-64px.png" alt="More options" />
       </button>
     </div>
 
@@ -139,4 +139,5 @@ fileInput.addEventListener("change", (e) => {
 
 // -------------------- INIT --------------------
 loadPosts();
+
 
