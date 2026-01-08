5ae66df6571622f5aad023b0c113f0053f93c8ce
yeomofficial <yeomapp@gmail.com>
2026-01-08 22:22:30 +0530

    Refactor Firebase setup and post creation logic

--- home.js
+++ home.js
@@ -36,7 +36,7 @@ function createPost({ username, imageUrl }) {
     <span class="username">${username}</span>
 
     <button class="post-menu" aria-label="Post options">
-  <img src="more_horiz_25dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="" class="post-menu-icon">
+  <img src="three-dots-512px.png" alt="" class="post-menu-icon">
 </button>
   </div>
 
@@ -139,6 +139,7 @@ fileInput.addEventListener("change", (e) => {
 
 // -------------------- INIT --------------------
 loadPosts();
+
 
 
 
