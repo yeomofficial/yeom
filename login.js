f2e237a17c98fa5c71072f79a2a39f5be51327e5
yeomofficial <yeomapp@gmail.com>
2026-02-07 11:23:10 +0530

    Import logEvent from analytics.js

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import {
   getAuth,
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { logEvent } from "./analytics.js";
 
 /* ================= Firebase Config ================= */
 const firebaseConfig = {
