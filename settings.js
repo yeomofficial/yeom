779a2ecbf09a613786dc2df3180d1c89881582f5
yeomofficial <yeomapp@gmail.com>
2026-02-08 12:50:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,22 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { logEvent } from "./analytics.js"; 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC1O-WVb95Z77o2JelptaZ8ljRPdNVDIeY",
-  authDomain: "yeom-official.firebaseapp.com",
-  projectId: "yeom-official",
-  storageBucket: "yeom-official.appspot.com",
-  messagingSenderId: "285438640273",
-  appId: "1:285438640273:web:7d91f4ddc24536a3c5ff30",
-  measurementId: "G-EBXHHN5WFK",
-};
+import { auth } from "./fbase.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { logEvent } from "./analytics.js";
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Optional: show messages to user
 function showMessage(text, isError = false) {
   const box = document.getElementById("message-box");
   if (!box) return;
@@ -26,13 +12,12 @@ function showMessage(text, isError = false) {
   box.style.display = "block";
 }
 
-// Logout function
 window.logout = async function () {
   try {
     const uid = auth.currentUser?.uid;
 
     if (uid) {
-      logEvent("user_logged_out", uid); // KPI tracked
+      await logEvent("user_logged_out", uid); 
     }
 
     await signOut(auth);
