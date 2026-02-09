7368a812dfe51dafd376deb8ea7b9075c127bc7a
yeomofficial <yeomapp@gmail.com>
2026-02-09 10:21:07 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -147,3 +147,16 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// -------------------- BLOCK IMAGE CONTEXT MENU --------------------
+document.addEventListener("contextmenu", (e) => {
+  if (e.target.closest("input, textarea")) return;
+  e.preventDefault();
+});
+
+document.addEventListener("dragstart", (e) => {
+  if (e.target.tagName === "IMG") {
+    e.preventDefault();
+  }
+});
+
