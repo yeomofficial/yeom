a8accef2cfb14bde9cb625578ad2ed12ace3a04f
yeomofficial <yeomapp@gmail.com>
2026-02-09 10:21:47 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -165,4 +165,15 @@ form.addEventListener("submit", async (e) => {
 });
 
 
+// -------------------- BLOCK IMAGE CONTEXT MENU --------------------
+document.addEventListener("contextmenu", (e) => {
+  if (e.target.closest("input, textarea")) return;
+  e.preventDefault();
+});
+
+document.addEventListener("dragstart", (e) => {
+  if (e.target.tagName === "IMG") {
+    e.preventDefault();
+  }
+});
 
