863183b325132ed51c0f6c9d304c83658cbdf277
yeomofficial <yeomapp@gmail.com>
2026-02-23 14:23:25 +0530

    Update mywardrobe.js

--- mywardrobe.js
+++ mywardrobe.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+
 import {
 getFirestore,
 collection,
@@ -9,8 +11,19 @@ getAuth,
 onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 
-const db = getFirestore();
-const auth = getAuth();
+const firebaseConfig = {
+  apiKey: "AIzaSyC1O-WVb95Z77o2JelptaZ8ljRPdNVDIeY",
+  authDomain: "yeom-official.firebaseapp.com",
+  projectId: "yeom-official",
+  storageBucket: "yeom-official.appspot.com",
+  messagingSenderId: "285438640273",
+  appId: "1:285438640273:web:7d91f4ddc24536a3c5ff30"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 const container = document.getElementById("myWardrobeContainer");
 
