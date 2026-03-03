ce92fe748c58c8957c39e0ee2a4f1ab4f11bb151
yeomofficial <yeomapp@gmail.com>
2026-03-03 07:52:50 +0530

    Create clothesdata.js

---
+++ clothesdata.js
@@ -0,0 +1,73 @@
+const clothes = [
+  {
+    id: "white_tshirt",
+    name: "White T-Shirt",
+    category: "top",
+    gender: "unisex",
+    style: "casual",
+    color: "white",
+    occasion: ["daily", "casual", "travel"],
+    image: "wardrobe/white_tshirt.jpg"
+  },
+  {
+    id: "blue_jeans",
+    name: "Blue Denim Jeans",
+    category: "bottom",
+    gender: "unisex",
+    style: "casual",
+    occasion: ["daily", "casual", "travel"],
+    color: "blue",
+    image: "wardrobe/blue_denimjeans.jpg"
+  },
+  {
+    id: "white_sneakers",
+    name: "White Converse",
+    category: "shoes",
+    gender: "men",
+    style: "casual",
+    occasion: ["daily", "casual", "travel", "college"],
+    color: "white",
+    image: "wardrobe/white_converse.jpg"
+  },
+  {
+    id: "w-btn-dwn-shirt-white",
+    name: "Button Down Shirt White",
+    category: "top",
+    gender: "women",
+    style: "formal",
+    occasion: ["formal", "office", "smart-casual"],
+    color: "white",
+    image: "wardrobe/women-btndown-white.jpg"
+  },
+  {
+    id: "black-boots",
+    name: "Black Chelsea",
+    category: "shoes",
+    gender: "men",
+    style: "formal",
+    color: "black",
+    occasion: ["formal", "smart-casual", "date", "evening"],
+    image: "wardrobe/men-chelseaboots-black.jpg"
+  },
+  {
+    id: "w-wideleg-pants-black",
+    name: "Wide Leg Pants Black",
+    category: "bottom",
+    gender: "women",
+    style: "formal",
+    occasion: ["formal", "office", "smart-casual"],
+    color: "black",
+    image: "wardrobe/w-wide-legpants-black.jpg"
+  },
+  {
+    id: "black-balletflat",
+    name: "Black Ballet Flat",
+    category: "shoes",
+    gender: "women",
+    style: "formal",
+    color: "black",
+    occasion: ["formal", "smart-casual", "date", "evening"],
+    image: "wardrobe/w-balletflats-black.jpg"
+  }
+];
+
