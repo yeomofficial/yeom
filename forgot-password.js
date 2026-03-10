1d63a4f2d5be248983d982f1e3a83f2181333312
yeomofficial <yeomapp@gmail.com>
2026-03-10 22:10:00 +0530

    Update forgot-password.js

--- forgot-password.js
+++ forgot-password.js
@@ -1,19 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-import { getAuth, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC1O-WVb95Z77o2JelptaZ8ljRPdNVDIeY",
-  authDomain: "yeom-official.firebaseapp.com",
-  projectId: "yeom-official",
-  storageBucket: "yeom-official.appspot.com",
-  messagingSenderId: "285438640273",
-  appId: "1:285438640273:web:7d91f4ddc24536a3c5ff30",
-  measurementId: "G-EBXHHN5WFK",
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
+import { auth } from "./fbase.js";
+import { sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 // Message display function
 function showMessage(text, isError = false) {
   const box = document.getElementById("message-box");
@@ -52,3 +38,4 @@ document.getElementById("reset-btn").addEventListener("click", () => {
       }
     });
 });
+
