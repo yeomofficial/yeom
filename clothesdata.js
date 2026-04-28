c618de862cc6bc34c5f826b8a2bb4a7819db91a6
yeomofficial <yeomapp@gmail.com>
2026-04-28 23:51:17 +0530

    Update image URL for white t-shirt

--- clothesdata.js
+++ clothesdata.js
@@ -10,7 +10,7 @@ export const clothes = [
     style: "casual",
     color: "white",
     occasion: ["daily", "casual", "travel"],
-    image: "wardrobe/white_tshirt.jpg"
+    image: "https://res.cloudinary.com/dwauleaof/image/upload/v1777400412/white_tshirt_pjyhjf.jpg"
   },
   {
   id: "black_tshirt",
