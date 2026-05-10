5731e26883137f781aed18335701af2d248a600e
yeomofficial <yeomapp@gmail.com>
2026-05-10 19:10:53 +0530

    Update wardrobe.js

--- wardrobe.js
+++ wardrobe.js
@@ -283,7 +283,7 @@ function loadClothes() {
 
     card.innerHTML = `
       <div class="img-wrapper">
-        <img src="\( {item.image}" alt=" \){item.name}" />
+        <img src="${item.image}" alt="${item.name}" />
         <button class="add-btn \( {isInWardrobe ? 'added' : ''}" data-id=" \){item.id}">
           <span class="icon plus">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
